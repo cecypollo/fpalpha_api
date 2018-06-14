4c6e4dc600669dabfe05883604d422b80f6d3358
Cecilia García de León <ceciliagarciadeleon@MacBook-Pro-de-Cecilia.local>
2018-06-14 13:45:42 -0500

    Added a custom user profile model, manager and migrations.

--- src/fpalpha/fpalpha/settings.py
+++ src/fpalpha/fpalpha/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'fpalpha_api'
+    'fpalpha_api',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL='fpalpha_api.UserProfile'
